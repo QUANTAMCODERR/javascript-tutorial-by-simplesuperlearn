7d0c6165c5221aa07d0a525e3330620deed97375
tushar gujjanwar <tushargujjanwar@gmail.com>
2024-09-21 18:09:12 +0530

    javascript tutorial with SuperSimpldDev

---
+++ operator.js
@@ -0,0 +1,28 @@
+// const result = true ? 'truthy' : 'falsy';
+// console.log(result);
+
+// //short cut of 
+// let result1;
+
+// if (condition) {
+//   result1 = 'truthy';
+
+// }
+
+// else {
+//   result1 = 'falsy';
+// }
+
+// Guard Operator
+
+
+// false && console.log("hello");
+// const message =1 && console.log("hello");
+// // shortcut
+// let message1
+// if(condition){
+//   message = 'hello';
+// }
+
+const currency = undefined || 'usd';
+console.log(currency);
